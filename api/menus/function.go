48ac93a8a1fc0d4ed6b5f6f0ac5dd40462e3dbde
nattawatsupangsarn015 <superpower-max@hotmail.com>
2023-01-04 21:23:31 +0700

    [ADD]✨ create menus with go driver

--- api/menus/function.go
+++ api/menus/function.go
@@ -2,12 +2,17 @@ package menus
 
 import (
 	"example/service-hiwjung-project/controller"
+	"example/service-hiwjung-project/responses"
 )
 
 func GetAllMenus() string {
 	return controller.GetAllMenus()
 }
 
-func CreateMenu(menu interface{}) (interface{}, error) {
+func CreateMenu(menu responses.MenuRequestCreate) (interface{}, error) {
+	err := controller.CreateMenu(menu)
+	if err != nil {
+		return nil, err
+	}
 	return menu, nil
 }
